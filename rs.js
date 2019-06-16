f54c216ddc5a1967b63e2effc1810f5bb64ad68e
RobertBedrosian <chrisx32@gmail.com>
2019-06-15 20:59:58 -0700

    initial

---
+++ rs.js
@@ -0,0 +1,9 @@
+console.log("hello");
+window.addEventListener("load", onLoad(), false);
+
+function onLoad(){
+    let box = document.getElementById("box");
+    box.addEventListener("mousedown", (event)=>{
+        if (event.clientX == box.)
+    });
+}
