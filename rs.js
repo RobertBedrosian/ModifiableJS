2b7c7cf5edcdb22ad9fbcb6ee8d77968e6b4720c
RobertBedrosian <chrisx32@gmail.com>
2019-06-17 22:35:52 -0700

    resizeable works! draggable works!

--- rs.js
+++ rs.js
@@ -89,9 +89,21 @@ function Resizeable(box){
         else if(event.offsetX <= 0 && event.offsetY >= parseInt(boxStyle.height)){
             document.onmousemove = resizeBl;
         }
-        else if(event.offsetX <= parseInt(boxStyle.width)
+        else if(event.offsetX >= parseInt(boxStyle.width)
         && event.offsetY >= parseInt(boxStyle.height)){
             document.onmousemove = resizeBr;
+        }
+        else if(event.offsetX <= 0){
+            document.onmousemove = resizeLeft;
+        }
+        else if(event.offsetY <= 0){
+            document.onmousemove = resizeTop;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)){
+            document.onmousemove = resizeRight;
+        }
+        else if(event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBottom;
         }
         document.onmouseup = stopResize;
     }
@@ -99,7 +111,6 @@ function Resizeable(box){
         event = event || window.event;
         event.preventDefault();
         event.stopPropagation();
-        document.documentElement.style.cursor = "nwse-resize";
         newX = event.clientX - origX;
         newY = event.clientY - origY;
         origX = event.clientX;
@@ -134,7 +145,6 @@ function Resizeable(box){
         event = event || window.event;
         event.preventDefault();
         event.stopPropagation();
-        document.documentElement.style.cursor = "nesw-resize";
         newX = event.clientX - origX;
         newY = event.clientY - origY;
         origX = event.clientX;
@@ -168,7 +178,6 @@ function Resizeable(box){
         event = event || window.event;
         event.preventDefault();
         event.stopPropagation();
-        document.documentElement.style.cursor = "nesw-resize";
         newX = event.clientX - origX;
         newY = event.clientY - origY;
         origX = event.clientX;
@@ -202,7 +211,6 @@ function Resizeable(box){
         event = event || window.event;
         event.preventDefault();
         event.stopPropagation();
-        document.documentElement.style.cursor = "nesw-resize";
         newX = event.clientX - origX;
         newY = event.clientY - origY;
         origX = event.clientX;
@@ -229,7 +237,82 @@ function Resizeable(box){
         else{
             box.style.width = (parseInt(boxStyle.width) + newX) + "px"
         }
+    }
+    function resizeLeft(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        origX = event.clientX;
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minWidth) == parseInt(boxStyle.width))){
+            if(newX <= 0 && event.clientX < parseInt(boxStyle.left)){
+                box.style.left = event.clientX + "px";
+                box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+            }
+        }
+        else{
+            box.style.left = event.clientX + "px";
+            box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+        }
 
+    }
+    function resizeTop(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newY = event.clientY - origY;
+        origY = event.clientY;
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minHeight) == parseInt(boxStyle.height))){
+            prevTop = parseInt(boxStyle.top);
+            if(newY <= 0 && event.clientY < parseInt(boxStyle.top)){
+                box.style.top = event.clientY + "px";
+                box.style.height = 
+                (parseInt(window.getComputedStyle(box).height) - newY) + "px";
+            }
+        }
+        else{
+            box.style.top = event.clientY + "px";
+            box.style.height = (parseInt(boxStyle.height) - newY) + "px";
+        }
+    }
+    function resizeRight(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newX = event.clientX - origX;
+        origX = event.clientX;
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minWidth) == parseInt(boxStyle.width))){
+            if(newX >= 0 && event.clientX > parseInt(boxStyle.left) +
+            parseInt(boxStyle.width)){
+                box.style.width = (parseInt(boxStyle.width) + newX) + "px"
+            }
+        }
+        else{
+            box.style.width = (parseInt(boxStyle.width) + newX) + "px"
+        }
+
+    }
+    function resizeBottom(event){
+        event = event || window.event;
+        event.preventDefault();
+        event.stopPropagation();
+        newY = event.clientY - origY;
+        origY = event.clientY;
+        //edge case, if box is at minimum specs, we want a certain behaviour
+        if((parseInt(boxStyle.minHeight) == parseInt(boxStyle.height))){
+            prevTop = parseInt(boxStyle.top);
+            if(newY >= 0 && event.clientY > parseInt(boxStyle.top) +
+            parseInt(boxStyle.height)){
+                box.style.height = 
+                (parseInt(window.getComputedStyle(box).height) + newY) + "px";
+            }
+        }
+        else{
+            box.style.height = (parseInt(boxStyle.height) + newY) + "px";
+        }
     }
     function stopResize(event){
         document.onmousemove = null;
