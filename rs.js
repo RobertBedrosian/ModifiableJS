717e12a1e8fd2dcf79b21196bccc60328fb8c449
RobertBedrosian <chrisx32@gmail.com>
2019-06-17 17:56:52 -0700

    resize and drag work

--- rs.js
+++ rs.js
@@ -1,9 +1,136 @@
-console.log("hello");
-window.addEventListener("load", onLoad(), false);
+window.addEventListener("load", onLoad, false);
 
 function onLoad(){
-    let box = document.getElementById("box");
+    Draggable(document.getElementById('box'));
+    Resizeable(document.getElementById('box'));
+}
+
+function Draggable(box){
+    var origX, origY, newX, newY = 0;
     box.addEventListener("mousedown", (event)=>{
-        if (event.clientX == box.)
+        event = event || window.event;
+        event.preventDefault();
+        origX = event.clientX;
+        origY = event.clientY;
+        document.onmousemove = boxMove;
+        document.onmouseup = stopBoxMove;
     });
+    function boxMove(event){
+        event = event || window.event;
+        event.preventDefault();
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.top = (box.offsetTop + newY) + "px";
+        box.style.left = (box.offsetLeft + newX) + "px";
+    }
+    function stopBoxMove(){
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+function Resizeable(box){
+    box.addEventListener("mousemove", mousemove);
+    var boxStyle = window.getComputedStyle(box);
+    var origX, origY, newX, newY = 0;
+    function mousemove(event){
+        if (event.offsetX <= 0 && event.offsetY <= 0){
+            box.style.cursor = "nwse-resize"
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY <=0){
+            box.style.cursor = "nesw-resize"
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX <= 0 && event.offsetY >= parseInt(boxStyle.height)){
+            box.style.cursor = "nesw-resize"
+            document.onmousedown = resize;
+        }
+        else if(event.offsetX <= parseInt(boxStyle.width)
+        && event.offsetY >= parseInt(boxStyle.height)){
+            box.style.cursor = "nwse-resize"
+            document.onmousedown = resize;
+        }
+        else if(!document.onmousemove){
+            box.style.cursor = "move";
+            document.onmousedown = null;
+        }
+    }
+    function resize(event){
+        event = event || window.event;
+        event.preventDefault();
+        origX = event.clientX;
+        origY = event.clientY;
+        if (event.offsetX <= 0 && event.offsetY <= 0){
+            document.onmousemove = resizeTl;
+        }
+        else if(event.offsetX >= parseInt(boxStyle.width)
+        && event.offsetY <=0){
+            document.onmousemove = resizeTr;
+        }
+        else if(event.offsetX <= 0 && event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBl;
+        }
+        else if(event.offsetX <= parseInt(boxStyle.width)
+        && event.offsetY >= parseInt(boxStyle.height)){
+            document.onmousemove = resizeBr;
+        }
+        document.onmouseup = stopResize;
+    }
+    function resizeTl(event){
+        event = event || window.event;
+        event.preventDefault();
+        document.documentElement.style.cursor = "nwse-resize";
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.top = (box.offsetTop + newY) + "px";
+        box.style.left = (box.offsetLeft + newX) + "px";
+        box.style.height = (parseInt(boxStyle.height) - newY) + "px";
+        box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+    }
+    function resizeTr(event){
+        event = event || window.event;
+        event.preventDefault();
+        document.documentElement.style.cursor = "nesw-resize";
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.top = (box.offsetTop + newY) + "px";
+        box.style.height = (parseInt(window.getComputedStyle(box).height) - newY) + "px";
+        box.style.width = (parseInt(window.getComputedStyle(box).width) + newX) + "px"
+    }
+    function resizeBl(event){
+        event = event || window.event;
+        event.preventDefault();
+        document.documentElement.style.cursor = "nesw-resize";
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.left = (box.offsetLeft + newX) + "px";
+        box.style.height = (parseInt(boxStyle.height) + newY) + "px";
+        box.style.width = (parseInt(boxStyle.width) - newX) + "px"
+    }
+    function resizeBr(event){
+        event = event || window.event;
+        event.preventDefault();
+        document.documentElement.style.cursor = "nesw-resize";
+        newX = event.clientX - origX;
+        newY = event.clientY - origY;
+        origX = event.clientX;
+        origY = event.clientY;
+        box.style.height = (parseInt(boxStyle.height) + newY) + "px";
+        box.style.width = (parseInt(boxStyle.width) + newX) + "px"
+    }
+    function stopResize(event){
+        document.onmousemove = null;
+        document.onmouseup = null;
+        document.documentElement.style.cursor = "";
+
+    }
 }
